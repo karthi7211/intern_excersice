6c1a078f8d60bdf0662bc8983c7345b8fd8e880d
PRADEEP <pradeepkumaran122006@gmail.com>
2025-12-07 19:51:50 +0530

    Add print statement for Pradeep's edit

--- Javap.java
+++ Javap.java
@@ -2,5 +2,6 @@ class Javap{
     public static void main(String args[]){
         System.out.println("Hello world.");
         System.out.println(" ganesh edited ");
+        System.out.println("Pradeep edited");
     }
 }
